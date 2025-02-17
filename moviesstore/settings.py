ae84d963f67d7c314144c1849155c9fab003b845
dman4703 <doquinn32@gmail.com>
2025-02-16 21:03:28 -0500

    added password reset functionality, also added basic templates for resetting passwords

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -116,6 +116,20 @@ USE_I18N = True
 
 USE_TZ = True
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
