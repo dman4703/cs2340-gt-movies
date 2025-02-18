bd6469e5624244919908a6489144333fd67e5920
dman4703 <doquinn32@gmail.com>
2025-02-18 12:15:25 -0500

    revamped home page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from movies.models import Movie
+from django.db.models import Q
+
+
 def index(request):
-    template_data = {}
-    template_data['title'] = 'Movies Store'
+    # Select 3 random movies that have either an uploaded image or an image URL.
+    featured_movies = Movie.objects.filter(
+        Q(image__isnull=False) | Q(image_url__isnull=False)
+    ).order_by('?')[:3]
+
+    template_data = {
+        'title': 'Movies Store',
+        'featured_movies': featured_movies,
+    }
     return render(request, 'home/index.html', {'template_data': template_data})
+
 def about(request):
     template_data = {}
     template_data['title'] = 'About'
